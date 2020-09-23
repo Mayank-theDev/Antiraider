3ce6e52dac8539657dcc5deea5eb63870e9b8bab
Mayank <68636590+HelpingHandOfficial@users.noreply.github.com>
2020-09-23 18:40:06 +0530

    Update infractions.js

--- commands/infractions.js
+++ commands/infractions.js
@@ -5,12 +5,12 @@ moment.locale('fr')
  
 module.exports = {
     run: async (message, args, client) => {
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You dont have enough perms to use this command.')
         const member = message.mentions.members.first()
-        if (!member) return message.channel.send('Veuillez mentionner le membre dont voir les warns.')
-        if (!client.db.warns[member.id]) return message.channel.send('Ce membre n\'a aucun warn.')
+        if (!member) return message.channel.send('Please mention the member whose warnings you want to see')
+        if (!client.db.warns[member.id]) return message.channel.send('This member doesnt have any warnings')
         message.channel.send(new Discord.MessageEmbed()
-            .setDescription(`**Total de warns :** ${client.db.warns[member.id].length}\n\n__**10 derniers warns**__\n\n${client.db.warns[member.id].slice(0, 10).map((warn, i) => `**${i + 1}.** ${warn.reason}\nSanctionné ${moment(warn.date).fromNow()} par <@!${warn.mod}>`).join('\n\n')}`))
+            .setDescription(`**Total warns :** ${client.db.warns[member.id].length}\n\n__**10 Latest warns**__\n\n${client.db.warns[member.id].slice(0, 10).map((warn, i) => `**${i + 1}.** ${warn.reason}\nSanctioned ${moment(warn.date).fromNow()} par <@!${warn.mod}>`).join('\n\n')}`))
     },
     name: 'infractions',
     guildOnly: true
