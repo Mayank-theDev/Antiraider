91fe59a655846a0e5a1e8fb25a23799ca0212dbb
Mayank <68636590+HelpingHandOfficial@users.noreply.github.com>
2020-09-23 18:55:47 +0530

    Update unlock.js

--- commands/unlock.js
+++ commands/unlock.js
@@ -2,12 +2,12 @@ const fs = require('fs')
  
 module.exports = {
     run: (message, args, client) => {
-        if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
+        if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send('You dont have perms to use this command')
         const channel = message.mentions.channels.first() || message.channel
-        if (!client.db.lockedChannels.includes(channel.id)) return message.channel.send('Ce salon n\'est pas vérrouillé.')
+        if (!client.db.lockedChannels.includes(channel.id)) return message.channel.send('This channel is already unlocked')
         client.db.lockedChannels.splice(client.db.lockedChannels.indexOf(channel.id), 1)
         fs.writeFileSync('./db.json', JSON.stringify(client.db))
-        message.channel.send('Ce salon a été déverrouillé !')
+        message.channel.send('This channel is now unlocked !')
     },
     name: 'unlock',
     guildOnly: true
