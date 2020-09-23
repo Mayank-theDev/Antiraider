e25f452d679cc9e7e7a055417d77b83659ba623d
Mayank <68636590+HelpingHandOfficial@users.noreply.github.com>
2020-09-23 18:43:43 +0530

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,14 +1,14 @@
 module.exports = {
     run: async (message, args) => {
-        if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
+        if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send('You dont have perms to use this command.')
         const member = message.mentions.members.first()
-        if (!member) return message.channel.send('Veuillez mentionner le membre à exclure.')
-        if (member.id === message.guild.ownerID) return message.channel.send('Vous ne pouvez pas exclure le propriétaire du serveur.')
-        if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('Vous ne pouvez pas exclure ce membre.')
-        if (!member.kickable) return message.channel.send('Le bot ne peut pas exclure ce membre.')
-        const reason = args.slice(1).join(' ') || 'Aucune raison fournie'
+        if (!member) return message.channel.send('Please mention the member to kick.')
+        if (member.id === message.guild.ownerID) return message.channel.send('You cant kick the server owner.')
+        if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('You cant kick this member.')
+        if (!member.kickable) return message.channel.send('The bot cant kick this member.')
+        const reason = args.slice(1).join(' ') || 'No reason provided'
         await member.kick(reason)
-        message.channel.send(`${member.user.tag} a été exclu !`)
+        message.channel.send(`${member.user.tag} was kicked from server! !`)
     },
     name: 'kick',
     guildOnly: true
