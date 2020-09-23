551b7b0fc166c049c20230bc110f0c1442f0c391
Mayank <68636590+HelpingHandOfficial@users.noreply.github.com>
2020-09-23 18:26:08 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,14 +1,14 @@
 module.exports = {
     run: async (message, args) => {
-        if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
+        if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('You do not have permission to use this command.')
         const member = message.mentions.members.first()
-        if (!member) return message.channel.send('Veuillez mentionner le membre à bannir.')
-        if (member.id === message.guild.ownerID) return message.channel.send('Vous ne pouvez pas bannir le propriétaire du serveur.')
-        if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('Vous ne pouvez pas Bannir ce membre.')
-        if (!member.bannable) return message.channel.send('Le bot ne peut pas bannir ce membre.')
+        if (!member) return message.channel.send('Please mention the member to be banned.')
+        if (member.id === message.guild.ownerID) return message.channel.send('You cannot ban the server owner.')
+        if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('You cant ban this member')
+        if (!member.bannable) return message.channel.send('The bot cant ban this member')
         const reason = args.slice(1).join(' ') || 'Aucune raison fournie'
         await member.ban({reason})
-        message.channel.send(`${member.user.tag} a été banni !`)
+        message.channel.send(`${member.user.tag} was banned, xD !`)
     },
     name: 'ban',
     guildOnly: true
