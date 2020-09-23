6a4104e15bac0f3d7867f88861741cea9eedb390
Mayank <68636590+HelpingHandOfficial@users.noreply.github.com>
2020-09-23 18:47:56 +0530

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,12 +1,12 @@
 module.exports = {
     run: async (message, args) => {
-        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Looks like you dont have perms.')
         const member = message.mentions.members.first()
-        if (!member) return message.channel.send('Veuillez mentionner le membre à mute.')
-        if (member.id === message.guild.ownerID) return message.channel.send('Vous ne pouvez mute le propriétaire du serveur.')
-        if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('Vous ne pouvez pas mute ce membre.')
-        if (!member.manageable) return message.channel.send('Le bot ne peut pas mute ce membre.')
-        const reason = args.slice(1).join(' ') || 'Aucune raison fournie.'
+        if (!member) return message.channel.send('Mention a member you want to mute.')
+        if (member.id === message.guild.ownerID) return message.channel.send('You cant mute the server owner.')
+        if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('You cant mute this member.')
+        if (!member.manageable) return message.channel.send('The bot cant mute this member.')
+        const reason = args.slice(1).join(' ') || 'No reason was given.'
         let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted')
         if (!muteRole) {
             muteRole = await message.guild.roles.create({
@@ -22,7 +22,7 @@ module.exports = {
             }))
         }
         await member.roles.add(muteRole)
-        message.channel.send(`${member} a été mute !`)
+        message.channel.send(`${member} is muted! !`)
     },
     name: 'mute',
     guildOnly: true
