5f203e15095195140e3e5c4ee8471733cba810ec
Mayank <68636590+HelpingHandOfficial@users.noreply.github.com>
2020-09-23 18:34:37 +0530

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,8 +2,8 @@ module.exports = {
     run: async (message, args) => {
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You dont have enough perms to use this command')
         const count = args[0]
-        if (!/\d+/.test(count)) return message.channel.send('Veuillez indiquer un nombre de messages à supprimer.')
-        if (count < 1 || count > 99) return message.channel.send('Le nombre de message doit être compris entre 1 et 99.')
+        if (!/\d+/.test(count)) return message.channel.send('Please indicate a number of messages to delete/purge.')
+        if (count < 1 || count > 99) return message.channel.send('The number of messages should be between 1 and 99.')
         const { size } = await message.channel.bulkDelete(Number(count) + 1, true)
         message.channel.send(`${size - 1} messages ont été supprimés !`).then(sent => sent.delete({timeout: 5e3}))
     },
