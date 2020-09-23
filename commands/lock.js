93c2dcb9b664c8e6cc41aaeb9712466c5fec41f3
Mayank <68636590+HelpingHandOfficial@users.noreply.github.com>
2020-09-23 18:44:54 +0530

    Update lock.js

--- commands/lock.js
+++ commands/lock.js
@@ -2,12 +2,12 @@ const fs = require('fs')
  
 module.exports = {
     run: (message, args, client) => {
-        if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
+        if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.channel.send('You miss perms to use this command.')
         const channel = message.mentions.channels.first() || message.channel
-        if (client.db.lockedChannels.includes(channel.id)) return message.channel.send('Ce salon est déjà vérrouillé.')
+        if (client.db.lockedChannels.includes(channel.id)) return message.channel.send('This channel is already locked.')
         client.db.lockedChannels.push(channel.id)
         fs.writeFileSync('./db.json', JSON.stringify(client.db))
-        message.channel.send('Ce salon a été verrouillé !')
+        message.channel.send('This channel has been locked !')
     },
     name: 'lock',
     guildOnly: true
